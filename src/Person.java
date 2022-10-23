2cf19843f97e5515dd95ea73d4ce0000690c3f4e
Damir <klivecar@yandex.ru>
2022-10-23 19:19:45 +0300

    Initial commit

---
+++ src/Person.java
@@ -0,0 +1,42 @@
+public class Person {
+    protected String name;
+    protected String surname;
+    protected int age;
+
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + surname + ", " + age + " года.";
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getSurname() {
+        return surname;
+    }
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int wordsInSurname() {
+        String[] word = surname.split(" ");
+        int count = word.length;
+        return count;
+    }
+}
